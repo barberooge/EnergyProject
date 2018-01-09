555d925dacad81cce007601a78d25ea557f647c8
Mathieu Mo <mathieumonnin@me.com>
2018-01-09 11:21:31 +0100

    Adding Login Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+    
+  get 'sessions/new'
+
   get 'about', to: 'static_pages#about'
 
   get 'help', to: 'static_pages#help'
@@ -13,8 +17,12 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
 
   get 'edit', to: 'users#edit'
-
-  root 'static_pages#home'
+    
+  get    '/login',   to: 'sessions#new'
+    
+  post   '/login',   to: 'sessions#create'
+    
+  delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
