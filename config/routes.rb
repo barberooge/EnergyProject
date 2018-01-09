bca85ef7fb52e89e169831bab1e45e538cc483d6
Mathieu Mo <mathieumonnin@me.com>
2018-01-09 10:56:07 +0100

    Adding Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  get 'about', to: 'static_pages#about'
 
-  get 'static_pages/help'
+  get 'help', to: 'static_pages#help'
 
-  get 'static_pages/contact'
+  get 'contact', to: 'static_pages#contact'
 
-  get 'users/new'
+  resources :users
 
-  get 'users/edit'
+  get 'signup', to: 'users#new'
 
-  get 'users/show'
+  post '/signup',  to: 'users#create'
+
+  get 'edit', to: 'users#edit'
 
   root 'static_pages#home'
 
